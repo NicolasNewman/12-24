5d3f0a9366bc2e4d0624bc4b3123db8715a525bd
NicolasNewman <nanewman01@gmail.com>
2020-10-28 15:37:00 -0400

    updated styling, added button interaction

--- src/browserAction/main.js
+++ src/browserAction/main.js
@@ -1,3 +1,28 @@
 import './style.less';
 
-document.getElementById('myHeading').style.color = 'red';
+const btnEnable = document.querySelector('#btnEnable');
+const btnSettings = document.querySelector('#btnSettings');
+const enableText = document.querySelector('#enable-text');
+
+const btnGreen = 'btn-green';
+const btnRed = 'btn-red';
+
+console.log('main.js for action');
+console.log(btnEnable);
+console.log(btnSettings);
+console.log(enableText);
+
+btnEnable.onclick = (e) => {
+    console.log('CLICKED');
+    if (btnEnable.classList.contains(btnGreen)) {
+        btnEnable.classList.remove(btnGreen);
+        btnEnable.classList.add(btnRed);
+        enableText.innerHTML = 'disabled';
+    } else {
+        btnEnable.classList.remove(btnRed);
+        btnEnable.classList.add(btnGreen);
+        enableText.innerHTML = 'enabled';
+    }
+};
+
+btnSettings.onclick = (e) => {};
